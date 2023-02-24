e22b9eb815803cf386bbb2b358c92ca2b4b562ed
Saroosh421 <i190599@nu.edu.pk>
2023-02-24 21:25:56 +0500

    changed test cases

--- unittesting/test_cases.py
+++ unittesting/test_cases.py
@@ -1,3 +1,38 @@
+import unittest
+from key_generator.key_generator import generate
+
+class TestGenerate(unittest.TestCase):
+    def test_default_values(self):
+        # Test with default values
+        key = generate().get_key()
+        self.assertIsInstance(key, str)
+        self.assertRegex(key, r'^[0-9a-fA-F]{3,10}(-[0-9a-fA-F]{3,10}){4}$')
+
+    def test_custom_values(self):
+        # Test with custom values
+        key = generate(4, ':', 5, 8, 'char', 'all', ['$', '#'], seed=101).get_key()
+        self.assertIsInstance(key, str)
+        self.assertRegex(key, r'^[A-F\$#]{5,8}(:[A-F\$#]{5,8}){3}$')
+
+    def test_mixed_case(self):
+        # Test with mixed case
+        key = generate(6, '-', 3, 6, 'int', 'mix', ['+', '*']).get_key()
+        self.assertIsInstance(key, str)
+        self.assertRegex(key, r'^[0-9a-fA-F\+\*]{3,6}(-[0-9a-fA-F\+\*]{3,6}){5}$')
+
+    def test_invalid_values(self):
+        # Test with invalid values
+        with self.assertRaises(ValueError):
+            key = generate(type_of_value='invalid_value')
+        with self.assertRaises(ValueError):
+            key = generate(capital='invalid_value')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+'''
 import string
 import unittest
 from key_generator.key_generator import generate
@@ -51,3 +86,5 @@ class TestGenerate(unittest.TestCase):
         
 if __name__ == '__main__':
     unittest.main()
+
+'''
