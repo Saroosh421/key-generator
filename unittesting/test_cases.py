6cda3188637f94cd5cb6743b35501d5efbb8ed2b
Saroosh421 <i190599@nu.edu.pk>
2023-02-24 22:03:00 +0500

    pylint added 2

--- unittesting/test_cases.py
+++ unittesting/test_cases.py
@@ -1,5 +1,6 @@
 import unittest
 from key_generator.key_generator import generate
+
 class TestGenerate(unittest.TestCase):
     """
     Test cases for the generate function in the key_generator module.
@@ -11,6 +12,7 @@ class TestGenerate(unittest.TestCase):
         key = generate().get_key()
         self.assertIsInstance(key, str)
         self.assertRegex(key, r'^[0-9a-fA-F]{3,10}(-[0-9a-fA-F]{3,10}){4}$')
+
     def test_mixed_case(self):
         """
         Test with mixed case.
@@ -18,6 +20,7 @@ class TestGenerate(unittest.TestCase):
         key = generate(6, '-', 3, 6, 'int', 'mix', ['+', '*']).get_key()
         self.assertIsInstance(key, str)
         self.assertRegex(key, r'^[0-9a-fA-F\+\*]{3,6}(-[0-9a-fA-F\+\*]{3,6}){5}$')
+
     def test_invalid_values(self):
         """
         Test with invalid values.
@@ -26,5 +29,7 @@ class TestGenerate(unittest.TestCase):
             key = generate(type_of_value='invalid_value')
         with self.assertRaises(ValueError):
             key = generate(capital='invalid_value')
+
 if __name__ == '__main__':
     unittest.main()
+
