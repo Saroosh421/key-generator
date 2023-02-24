111f668c35f3877776e20eb84ad49fc8981dccaf
Saroosh421 <i190599@nu.edu.pk>
2023-02-24 23:16:34 +0500

    adding corrections 10

--- unittesting/test_cases.py
+++ unittesting/test_cases.py
@@ -1,6 +1,7 @@
 """test_cases.py"""
 import unittest
-import os, sys 
+import os
+import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from key_generate.key_generator import generate
 class TestGenerate(unittest.TestCase):
