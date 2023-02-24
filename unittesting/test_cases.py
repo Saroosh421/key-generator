4df4abe45d1d42700d8836c1b7948169cc5c44b0
Saroosh421 <i190599@nu.edu.pk>
2023-02-24 21:56:42 +0500

    adding corrections

--- unittesting/test_cases.py
+++ unittesting/test_cases.py
@@ -1,22 +1,37 @@
 import unittest
 from key_generator.key_generator import generate
+
+
 class TestGenerate(unittest.TestCase):
-    """Test cases for the generate function."""
+    """
+    Test cases for the generate function in the key_generator module.
+    """
+    
     def test_default_values(self):
-        """Test generate function with default values."""
+        """
+        Test with default values.
+        """
         key = generate().get_key()
         self.assertIsInstance(key, str)
         self.assertRegex(key, r'^[0-9a-fA-F]{3,10}(-[0-9a-fA-F]{3,10}){4}$')
+
     def test_mixed_case(self):
-        """Test generate function with mixed case."""
+        """
+        Test with mixed case.
+        """
         key = generate(6, '-', 3, 6, 'int', 'mix', ['+', '*']).get_key()
         self.assertIsInstance(key, str)
         self.assertRegex(key, r'^[0-9a-fA-F\+\*]{3,6}(-[0-9a-fA-F\+\*]{3,6}){5}$')
+
     def test_invalid_values(self):
-        """Test generate function with invalid values."""
+        """
+        Test with invalid values.
+        """
         with self.assertRaises(ValueError):
-            generate(type_of_value='invalid_value')
+            key = generate(type_of_value='invalid_value')
         with self.assertRaises(ValueError):
-            generate(capital='invalid_value')
+            key = generate(capital='invalid_value')
+
+
 if __name__ == '__main__':
     unittest.main()
