adc36e3d79d20ba41e4f734dd12faa2613e5337f
Saroosh421 <i190599@nu.edu.pk>
2023-02-24 23:34:44 +0500

    adding corrections 11

--- unittesting/test_cases.py
+++ unittesting/test_cases.py
@@ -2,8 +2,8 @@
 import unittest
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from key_generate.key_generator import generate
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 class TestGenerate(unittest.TestCase):
     ("""
     Test cases for the generate function in the key_generator module.
