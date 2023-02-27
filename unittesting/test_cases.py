a5aecbdc2dd562af5c1e95f7fe2f1d7eb5f1af45
Saroosh421 <Saroosh421@users.noreply.github.com>
2023-02-27 18:18:03 +0000

    Merge saroosh branch

--- unittesting/test_cases.py
+++ unittesting/test_cases.py
@@ -3,32 +3,48 @@ import unittest
 import os
 import sys
 from key_generate.key_generator import generate
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
 class TestGenerate(unittest.TestCase):
-    ("""
+    (
+        """
     Test cases for the generate function in the key_generator module.
-    """)
+    """
+    )
+
     def test_default_values(self):
-        ("""
+        (
+            """
         Test with default values.
-        """)
+        """
+        )
         key = generate().get_key()
         self.assertIsInstance(key, str)
-        self.assertRegex(key, r'^[0-9a-fA-F]{3,10}(-[0-9a-fA-F]{3,10}){4}$')
+        self.assertRegex(key, r"^[0-9a-fA-F]{3,10}(-[0-9a-fA-F]{3,10}){4}$")
+
     def test_mixed_case(self):
-        ("""
+        (
+            """
         Test with mixed case.
-        """)
-        key = generate(6, '-', 3, 6, 'int', 'mix', ['+', '*']).get_key()
+        """
+        )
+        key = generate(6, "-", 3, 6, "int", "mix", ["+", "*"]).get_key()
         self.assertIsInstance(key, str)
-        self.assertRegex(key, r'^[0-9a-fA-F\+\*]{3,6}(-[0-9a-fA-F\+\*]{3,6}){5}$')
+        self.assertRegex(key, r"^[0-9a-fA-F\+\*]{3,6}(-[0-9a-fA-F\+\*]{3,6}){5}$")
+
     def test_invalid_values(self):
-        ("""
+        (
+            """
         Test with invalid values.
-        """)
+        """
+        )
         with self.assertRaises(ValueError):
-            generate(type_of_value='invalid_value')
+            generate(type_of_value="invalid_value")
         with self.assertRaises(ValueError):
-            generate(capital='invalid_value')
-if __name__ == '__main__':
+            generate(capital="invalid_value")
+
+
+if __name__ == "__main__":
     unittest.main()
